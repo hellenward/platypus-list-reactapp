a4a1137be35fab36cb9cfd8ce3a4cdc914f98e1c
Hellen Ward <hellenwellsward@gmail.com>
2020-01-12 11:29:24 +0000

    Created ToDoList component and refactored so ToDoItem is rendered within ToDoList

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Menu from "./components/menu.jsx";
 import InputBox from "./components/inputbox.jsx";
-import ToDoItem from "./components/todoitem.jsx";
+import ToDoList from "./components/todolist.jsx";
 
 function App() {
   return (
     <div className="App">
       <Menu />
       <InputBox />
-      <ToDoItem />
+      <ToDoList />
     </div>
   );
 }
---
+++ src/components/todolist.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ToDoItem from "./todoitem.jsx";
+
+const ToDoList = props => {
+  return (
+    <div>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+  );
+};
+
+export default ToDoList;
