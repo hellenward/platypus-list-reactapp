17c073441f6897d5e898fafc476bb6eedf4b558c
Hellen Ward <hellenwellsward@gmail.com>
2020-01-28 20:10:20 +0000

    Got input box working, made it into a controlled component

--- src/components/inputbox.jsx
+++ src/components/inputbox.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 
-const InputBox = () => {
-  return (
-    <div>
-      <input
-        type="text"
-        name="todo"
-        value="Add something to your list..."
-      ></input>
-    </div>
-  );
-};
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("Submitted thing was submitted: " + this.state.value);
+    event.preventDefalut();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter text here :
+          <input
+            type="text"
+            name="todo"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
+        </label>
+      </form>
+    );
+  }
+}
 
 export default InputBox;
